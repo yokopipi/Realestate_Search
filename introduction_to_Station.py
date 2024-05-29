9e7a686f9fc83210ab93c14aa2764df007f3e38a
yokopipi <165488818+yokopipi@users.noreply.github.com>
2024-05-29 19:14:22 +0900

    Delete introduction_to_Station.py

--- introduction_to_Station.py
+++
@@ -1,4 +0,0 @@
-def introduction_to_Station():
-    introduction_text = "ああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああああ"    
-    
-    return introduction_text
